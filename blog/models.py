3aec0dacc654528ec17a5fc4c4897329f0de31ea
rvbcia <dagryba@gmail.com>
2020-09-19 16:38:17 +0200

    Modified templates to display posts from db

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,6 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
-    is_shitpost = models.BooleanField(default=True)
 
     def publish(self):
         self.published_date = timezone.now()
